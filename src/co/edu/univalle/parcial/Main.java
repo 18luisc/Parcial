41684eab4f547659462ea68db88dd9bd2c0d6591
18luisc <129436183+18luisc@users.noreply.github.com>
2023-06-22 15:07:16 -0500

    Adición de Clases Necesarias y Archivos.txt

--- src/co/edu/univalle/parcial/Main.java
+++ src/co/edu/univalle/parcial/Main.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package parcial;
+package co.edu.univalle.parcial;
 
 /**
  *
  * @author User
  */
-public class Parcial {
+public class Main {
 
     /**
      * @param args the command line arguments
