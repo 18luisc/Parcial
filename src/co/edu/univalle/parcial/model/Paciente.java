4f872b044d602259fc81319bfb1f7e37a8c51676
18luisc <129436183+18luisc@users.noreply.github.com>
2023-06-22 16:58:32 -0500

    Actualización Clase Paciente

--- src/co/edu/univalle/parcial/model/Paciente.java
+++ src/co/edu/univalle/parcial/model/Paciente.java
@@ -9,5 +9,58 @@ package co.edu.univalle.parcial.model;
  * @author User
  */
 public class Paciente {
-    
+    private int id;
+    private String apellido;
+    private String nombre;
+    private int telefono;
+    private String direccion;
+
+    public Paciente(int id, String apellido, String nombre, int telefono, String direccion) {
+        this.id = id;
+        this.apellido = apellido;
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.direccion = direccion;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+        
 }
